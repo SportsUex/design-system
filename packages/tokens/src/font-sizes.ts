f222b9f2b07395b256c26229de229190d346d09c
Lucas Laurentino <laurentino14@outlook.com>
2023-03-14 21:43:17 -0300

    fix(tokens): fix font-sizes values

--- packages/tokens/src/font-sizes.ts
+++ packages/tokens/src/font-sizes.ts
@@ -1,8 +1,9 @@
 export const fontSizes = {
-  xs: '0.75rem',
-  sm: '1rem',
-  md: '1.3125rem',
-  lg: '1.75rem',
-  xl: '2.3125rem',
-  xxl: '3.125rem',
+  xs: '0.563rem',
+  sm: '0.75rem',
+  md: '1rem',
+  lg: '1.3125rem',
+  xl: '1.75rem',
+  xxl: '2.3125rem',
+  xxxl: '3.125rem',
 }
